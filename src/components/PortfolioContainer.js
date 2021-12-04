1f0ef65725afff3b874dd5013b651efada233d91
James Garinger <jkggaringer@gmail.com>
2021-12-04 15:19:47 -0600

    changing file name structure in src, adding some content to about me

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
-    return <Contact />;
+    return <Home />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <img></img>
+      <img></img>
+      <img></img>
+      <img></img>
+    </div>
+  );
+}
