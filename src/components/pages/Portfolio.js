82e4ff3d733b965bd5f0ef2be460b3840e67b130
James Garinger <jkggaringer@gmail.com>
2021-12-04 18:44:04 -0600

    adding image links and hrefs to projects in portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,13 +1,23 @@
 import React from 'react';
-
+import woImage from '../../../src/leverageWo.jpg'
+import WfImage from '../../../src/WFtracker.jpg'
 export default function Portfolio() {
+  
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <img></img>
-      <img></img>
-      <img></img>
-      <img></img>
+      <p>These are some projects I created with my groups while attending UT austin bootcamp</p>
+      
+      <a href="https://leveragewo.herokuapp.com/">
+        <img src={woImage} alt="LeverageWo Application Homepage"></img>
+      </a>
+      
+      <a href="https://chuck2076.github.io/Weather_Wildfire_App/">
+        <img src={WfImage} alt="Weather And Fire Tracker Application"></img>
+      </a>
+
+      <a><img></img></a>
+      <a><img></img></a>
     </div>
   );
 }
