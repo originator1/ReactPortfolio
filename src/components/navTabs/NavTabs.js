3c85aaf389930ea367726447eacf48d48d5d54af
James Garinger <jkggaringer@gmail.com>
2022-03-25 02:01:35 -0500

    updating page layouts and navigation styling, adding new project display

--- src/components/navTabs/NavTabs.js
+++ src/components/navTabs/NavTabs.js
@@ -1,39 +1,56 @@
 import React from 'react';
-import "./NavTabs.css";
-import { Navbar, Container } from 'react-bootstrap'
 
+import { Navbar, Container } from 'react-bootstrap'
+import "./NavTabs.css";
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
+
+   function handleNavToggle(e) {
+      
+      let myNav = window.document.getElementById('navv')
+      console.log(myNav.style)
+      if (myNav.style.display == "") {
+         console.log(myNav)
+         return myNav.style.display="none"
+         
+         
+      } else {
+          return myNav.style.display=""
+         
+
+      }
+   }
+
+
   return (
     
     
-      <Navbar expand="lg" variant="light" bg="none">
-         <Container  style={{justifyContent: 'space-around'}} className="linkBox">
-            <h1 href="#home"  onClick={() => handlePageChange('Home')} id="navName">James Garinger</h1>
-            <Navbar.Brand 
-            href="#home"
-            style={{color: "white", fontWeight: "bold", fontStyle: "italic"}}
-            onClick={() => handlePageChange('Home')}
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</Navbar.Brand>
-            
-           <Navbar.Brand  
-            href="#Portfolio"
-            style={{color: "white", fontWeight: "bold", fontStyle: "italic"}}
-            onClick={() => handlePageChange('Portfolio')}
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Work</Navbar.Brand>
-           
-           <Navbar.Brand 
-            href="#contact"
-            style={{color: "white", fontWeight: "bold", fontStyle: "italic"}}
-            onClick={() => handlePageChange('Contact')}
-            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</Navbar.Brand>
-         </Container>
-      </Navbar>
+     <Navbar expand="sm" className="mainNav">
+        <a href="#home" onClick={(e) => handleNavToggle(e)} class="pull-left iconBtn"><img src="./apple-touch-icon.png" style={{height: "7vh"}} /></a> 
+        <div className="linkBox" id="navv">
+         
+           <Navbar.Brand
+              href="#home"
+              style={{ fontStyle: "italic" }}
+              onClick={() => handlePageChange('Home')}
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</Navbar.Brand>
+
+           <Navbar.Brand
+              href="#Portfolio"
+              style={{ fontStyle: "italic" }}
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Work</Navbar.Brand>
+
+           <Navbar.Brand
+              href="#contact"
+              style={{ fontStyle: "italic" }}
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>About</Navbar.Brand>
+
+        </div>
+        
+     </Navbar>
     
   );
 
